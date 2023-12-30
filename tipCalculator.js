09d7296d5f26ec8bbba015ffc9d57d3f9e846249
DanTheDev27 <153468216+DanTheDev27@users.noreply.github.com>
2023-12-30 12:35:10 -0500

    Update tipCalculator.js

--- tipCalculator.js
+++ tipCalculator.js
@@ -8,6 +8,7 @@ function calculate() {
         var displayTotal = document.getElementById("total");
         var displayTipAmount = document.getElementById("tipAmount");
         var displayTotalDivided = document.getElementById("totalDivided");
+        var displayError = document.getElementById("error");
 
         var tipAmount = mealAmount * tipPercentage;
         var totalBill = mealAmount + tipAmount;
@@ -19,8 +20,25 @@ function calculate() {
         if (!totalPeople === "" || totalPeople> 1) {
             displayTotalDivided.innerText="Each person pays" + " $" + totalDivided.toFixed(2);
         }
-        //console.log(typeof totalPeople);
+
+        errorMessage= "You can not leave the fields empty";
+        // errorMessage.style.color= "red";
+        displayError.style.color= "red";
+        displayError.style.textAlign= "center";
+        displayError.style.fontSize= "130%";
+        if (document.getElementById("mealAmount").value==="",  document.getElementById("service").value==="", document.getElementById("numofPeople").value==="") {
+            displayTotal.innerText= "";
+            displayTipAmount.innerText= "";
+            displayTotalDivided.innerText= "";
+            displayError.innerText= errorMessage;
+
+        } else {
+            displayError.innerText= "";
+        }
+        
     })
+
+    
 }
 
 function erase() {
@@ -28,5 +46,10 @@ function erase() {
         document.getElementById("mealAmount").value="";
         document.getElementById("service").value="";
         document.getElementById("numofPeople").value="";
+        document.getElementById("total").innerText="";
+        document.getElementById("tipAmount").innerText="";
+        document.getElementById("totalDivided").innerText="";
+
+        
     })
 }
